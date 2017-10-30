4ca87543a6779c518aa009689484def4c9bdf6e7
Pozinzinho <32583639+Pozinzinho@users.noreply.github.com>
2017-10-30 20:11:59 -0200

    Revert "TarefaNavas"
    This reverts commit 92bcdd16f4c4e6e3e3a03096f287d11b9b7c80e5.

---
+++ app/src/main/java/com/example/manoel/wolfchatapp/Activity/TarefaNavas.java
@@ -0,0 +1,16 @@
+package com.example.manoel.wolfchatapp.Activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.manoel.wolfchatapp.R;
+
+public class TarefaNavas extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarefa_navas);
+    }
+
+}
